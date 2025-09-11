604378f48d8295568600be6ec6994932211e371f
Huicong967 <huicong.xu@connect.polyu.hk>
2025-09-12 00:34:44 +0800

    你的提交说明

---
+++ programing.py
@@ -0,0 +1,3 @@
+import requests
+from lxml import html
+import matplotlib.pyplot as plt
---
+++ scraping_utils.py
@@ -0,0 +1,9 @@
+# scraping_utils.py
+
+def get_url(url):
+    # 占位函数，请根据实际需求实现
+    return url
+
+def parse(content):
+    # 占位函数，请根据实际需求实现
+    return content
